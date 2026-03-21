a82788169feb22241eb0c790d7c09e003e8d79d1
wuehr1999 <wuehr1999>
2026-03-21 21:24:48 +0100

    prepare for docker

--- src/iot_backend/api_user/base.py
+++ src/iot_backend/api_user/base.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*- 
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import click
 from sqlmodel import Field, Session, SQLModel, create_engine, select, delete
@@ -17,13 +18,27 @@ def main(host: str):
     db = pg.PostgresConnector()
     
     app = FastAPI()
+    origins = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     @app.get('/temperature')
     def get_temperature():
         with Session(db.engine) as session:
-            statement = select(temp.Temperature)
-            temperatures = session.exec(statement).all()
-            return list(temperatures)
+            statement = select(temp.Temperature.dev_id).distinct('dev_id')
+            dev_ids = list(session.exec(statement).all())
+            data = []
+            for dev_id in dev_ids:
+                statement = select(temp.Temperature.celsius).where(temp.Temperature.dev_id == dev_id) 
+                temperatures = list(session.exec(statement).all())
+                data.append({"dev_id": dev_id, "celsius": temperatures})
+            return data
 
     @app.delete('/temperature')
     def clear_temperature():
@@ -33,4 +48,4 @@ def main(host: str):
             session.commit()
         return "{}"
 
-    uvicorn.run(app, host = host, port=5000)
+    uvicorn.run(app, host = host, port = 5000)
